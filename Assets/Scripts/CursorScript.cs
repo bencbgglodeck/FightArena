692d288756d07308061dd39a129c5403d7361b23
Benjamin Glodeck <glodeckbenjamin@gmail.com>
2018-08-27 18:45:49 +0200

    Cursor Hide and Texture

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start ()
+    {
+        Cursor.visible = true;
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            Cursor.visible = false;
+        }
+        else if (Input.GetMouseButtonUp(0) == true)
+        {
+            Cursor.visible = true;
+        }
+    }
+}
